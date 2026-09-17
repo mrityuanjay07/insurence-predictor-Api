679b1468b756fd8af3c8b2481e7e8f5e197a4e33
Mrityuanjay  Kumar kewat <mrityuanjaykewat@gmail.com>
2026-09-17 22:08:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -106,10 +106,14 @@ def predict_premium(data: UserInput):
         'occupation': data.occupation
     }])
 
-    prediction = model.predict(input_df)[0]
+    try:
 
-    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+            prediction = model.predict(input_df)[0]
 
+            return JSONResponse(status_code=200, content={'predicted_category': prediction})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e) )
 
 
 
