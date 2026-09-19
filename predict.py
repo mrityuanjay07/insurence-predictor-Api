0cd5d88566aa40ac3890acc188579b63fbf4757b
Mrityuanjay  Kumar kewat <mrityuanjaykewat@gmail.com>
2026-09-19 23:04:44 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import pandas as pd
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
+model_version = '1.0.0'
 def predict_output(user_input: dict):
 
     input_df=pd.DataFrame([user_input])
